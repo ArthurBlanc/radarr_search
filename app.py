dfce2250fd068722d374153c9abe5ae222b5d359
Arthur BLANC <72075460+arthurblanc@users.noreply.github.com>
2024-09-03 14:52:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,11 +46,51 @@ def trigger_search(instance):
     except Exception as e:
         logger.error(f"An unexpected error occurred while triggering search on {instance['url']}: {e}")
 
-def main():
-    logger.info("Starting script execution")
+def clear_blocklist(instance):
+    logger.info(f"Starting blocklist clearance for {instance['url']}")
+    url = f"{instance['url']}/api/v3/blocklist"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Api-Key": instance['api_key']
+    }
+
+    try:
+        response = requests.delete(url, headers=headers)
+        response.raise_for_status()
+        if response.status_code == 200:
+            logger.info(f"Successfully cleared blocklist on {instance['url']}")
+        else:
+            response_json = response.json()
+            logger.error(f"Failed to clear blocklist on {instance['url']}: {response.status_code}, {response_json}")
+    except requests.exceptions.HTTPError as http_err:
+        logger.error(f"HTTP error occurred while clearing blocklist on {instance['url']}: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        logger.error(f"Request error occurred while clearing blocklist on {instance['url']}: {req_err}")
+    except Exception as e:
+        logger.error(f"An unexpected error occurred while clearing blocklist on {instance['url']}: {e}")
+
+def main_startup():
+    logger.info("Starting script execution at container startup")
     for instance in RADARR_INSTANCES:
         logger.info(f"Processing instance: {instance['url']}")
         trigger_search(instance)
+        clear_blocklist(instance)
+    logger.info("Container startup script execution completed")
+
+def main():
+    task = os.getenv('TASK', 'search')
+    logger.info(f"Running task: {task}")
+    
+    if task == 'startup':
+        main_startup()
+    elif task == 'search':
+        for instance in RADARR_INSTANCES:
+            logger.info(f"Processing instance: {instance['url']}")
+            trigger_search(instance)
+    elif task == 'clear_blocklist':
+        for instance in RADARR_INSTANCES:
+            logger.info(f"Processing instance: {instance['url']}")
+            clear_blocklist(instance)
     logger.info("Script execution completed")
 
 if __name__ == "__main__":
