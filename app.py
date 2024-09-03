b590478aeaf5ffcf4e1e988d344fa28694786e0b
Arthur BLANC <72075460+arthurblanc@users.noreply.github.com>
2024-09-03 15:44:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 import logging
 import os
+from datetime import datetime
 
 # Setup logging configuration
 logging.basicConfig(
@@ -47,7 +48,7 @@ def trigger_search(instance):
         logger.error(f"An unexpected error occurred while triggering search on {instance['url']}: {e}")
 
 def clear_blocklist(instance):
-    logger.info(f"Starting blocklist clearance for {instance['url']}")
+    logger.info(f"Starting blocklist clearance for {instance['url']} at {datetime.now()}")
     url = f"{instance['url']}/api/v3/blocklist"
     headers = {
         "Content-Type": "application/json",
@@ -58,16 +59,16 @@ def clear_blocklist(instance):
         response = requests.delete(url, headers=headers)
         response.raise_for_status()
         if response.status_code == 200:
-            logger.info(f"Successfully cleared blocklist on {instance['url']}")
+            logger.info(f"Successfully cleared blocklist on {instance['url']} at {datetime.now()}")
         else:
             response_json = response.json()
-            logger.error(f"Failed to clear blocklist on {instance['url']}: {response.status_code}, {response_json}")
+            logger.error(f"Failed to clear blocklist on {instance['url']} at {datetime.now()}: {response.status_code}, {response_json}")
     except requests.exceptions.HTTPError as http_err:
-        logger.error(f"HTTP error occurred while clearing blocklist on {instance['url']}: {http_err}")
+        logger.error(f"HTTP error occurred while clearing blocklist on {instance['url']} at {datetime.now()}: {http_err}")
     except requests.exceptions.RequestException as req_err:
-        logger.error(f"Request error occurred while clearing blocklist on {instance['url']}: {req_err}")
+        logger.error(f"Request error occurred while clearing blocklist on {instance['url']} at {datetime.now()}: {req_err}")
     except Exception as e:
-        logger.error(f"An unexpected error occurred while clearing blocklist on {instance['url']}: {e}")
+        logger.error(f"An unexpected error occurred while clearing blocklist on {instance['url']} at {datetime.now()}: {e}")
 
 def main_startup():
     logger.info("Starting script execution at container startup")
